312ef5bbaa736bdfa17b8ee11b67d35caab61f38
AkinAguda <lordtitan7@gmail.com>
2021-10-02 13:34:48 +0100

    added gauss seidel implementations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { euler } from "./module";
+import { euler, gaussSeidel, gaussSeidel1 } from "./module";
 
 console.log(
   euler({
@@ -8,4 +8,30 @@ console.log(
     step: 0.1,
     limit: 2.5,
   })
+);
+
+console.log(
+  gaussSeidel(
+    [
+      (a, b, c, d) => (1 / 10) * (3 + 2 * b + c + d),
+      (a, b, c, d) => (1 / 10) * (15 + 2 * a + c + d),
+      (a, b, c, d) => (1 / 10) * (27 + a + b + d),
+      (a, b, c, d) => (1 / 10) * (-9 + a + b + 2 * c),
+    ],
+    [0, 0, 0, 0],
+    1000
+  )
+);
+
+console.log(
+  gaussSeidel1(
+    [
+      (a, b, c, d) => (1 / 10) * (3 + 2 * b + c + d),
+      (a, b, c, d) => (1 / 10) * (15 + 2 * a + c + d),
+      (a, b, c, d) => (1 / 10) * (27 + a + b + d),
+      (a, b, c, d) => (1 / 10) * (-9 + a + b + 2 * c),
+    ],
+    [0, 0, 0, 0],
+    21
+  )
 );
--- src/module.ts
+++ src/module.ts
@@ -8,20 +8,26 @@ interface EulerConfig {
   limit: number;
 }
 
-// Fixes floating point precision error
-const fixFP = (number: number) =>
-  Math.round((number + Number.EPSILON) * 100) / 100;
+const round = (num: number, precision: number) =>
+  Math.round((num + Number.EPSILON) * precision) / precision;
+
+const isPrecise = (x: number, y: number, precision: number) => {
+  return round(Math.abs(x - y), precision) <= 1 / precision;
+};
 
 export const euler = (config: EulerConfig) => {
   const memoYandDyDx = new Map<number, [number, number]>();
   const yNPlusOne = (x: number): number => {
-    const incX = fixFP(x + config.step);
+    const incX = round(x + config.step, 100);
     if (x === config.xInitial) {
       memoYandDyDx.set(x, [config.yInitial, config.dyDx(x, config.yInitial)]);
       return yNPlusOne(incX);
     }
-    const prev = memoYandDyDx.get(fixFP(x - config.step));
-    const y = fixFP(fixFP(prev![0]) + fixFP(config.step * prev![1]));
+    const prev = memoYandDyDx.get(round(x - config.step, 100));
+    const y = round(
+      round(prev![0], 100) + round(config.step * prev![1], 100),
+      100
+    );
     memoYandDyDx.set(x, [y, config.dyDx(x, y)]);
     if (x === config.limit) {
       return y;
@@ -30,4 +36,56 @@ export const euler = (config: EulerConfig) => {
     }
   };
   return yNPlusOne(config.xInitial);
+};
+
+/**
+ * This function is used for solving and estimating the
+ * values in set of simultaneous linear equations.
+ *
+ * In our case, we are using it to calculate the ddensity
+ * or anything really of the surrounding squares around a grid square
+ */
+export const gaussSeidel = (
+  functions: Array<(...params: number[]) => number>,
+  initialValues: number[],
+  precision: number,
+  finalPrecision = 10
+) => {
+  const initialValuesClone = [...initialValues];
+  const prevIterationValues: number[] = [];
+  let precisionCount = 0;
+  while (precisionCount < initialValues.length) {
+    precisionCount = 0;
+    initialValues.forEach((_, index) => {
+      initialValuesClone[index] = functions[index](...initialValuesClone);
+      if (prevIterationValues[index] !== undefined && prevIterationValues) {
+        if (
+          isPrecise(
+            prevIterationValues[index],
+            initialValuesClone[index],
+            precision
+          )
+        ) {
+          precisionCount += 1;
+        }
+      }
+      prevIterationValues[index] = initialValuesClone[index];
+    });
+  }
+  return initialValuesClone.map((v) => round(v, finalPrecision));
+};
+
+// This method gets the estimates after a certain number of iterations
+export const gaussSeidel1 = (
+  functions: Array<(...params: number[]) => number>,
+  initialValues: number[],
+  iter: number
+) => {
+  const initialValuesClone = [...initialValues];
+  for (let i = 0; i < iter; i++) {
+    initialValues.forEach((_, index) => {
+      initialValuesClone[index] = functions[index](...initialValuesClone);
+    });
+  }
+  return initialValuesClone;
 };
